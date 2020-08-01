bbc5470aa9694b8fec9521a947d5f6215f23ed47
challaCBN123 <challabhargava55@gmail.com>
2020-08-01 11:29:47 +0530

    Update

--- polyLineMe/ViewController.swift
+++ polyLineMe/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController{
     var baseUrl = "http://jsonplaceholder.typicode.com/comments"
     //"http://jsonplaceholder.typicode.com/albums"
     let endKey = ""
+    // MARK:- another API
+    let UrlConnection = "http://api.kivaws.org/v1/loans/search.json?status=fundraising"
     // MARK :- Zomato Api
      let baseURLString = "https://developers.zomato.com/api/v2.1/geocode?lat=16.515099&lon=80.632095"
        let apiKey = "c56ce01dafd40a68e12e336e50d91e0b"
@@ -27,15 +29,27 @@ class ViewController: UIViewController{
          }
     func getdata(){
         TomatoData.service.gettingData(baseUrl: baseUrl,seriveUrl:endKey, encoding:JSONEncoding.default, headerParameters: [:], view: self.view) { (responseData, error) in
-                   //print(responseData)
-                   self.arraySerach = responseData as! [[String : Any]]
-                   
-            print(self.arraySerach.count)
-            DispatchQueue.global().async {
-                DispatchQueue.main.async {
-                             self.tblView.reloadData() // refresh
-                           }
+print(responseData)
+            if responseData.count>0{
+                 self.arraySerach = responseData as! [[String : Any]]
+               
+            }else {
+               print(Result<Any, Error>.self)
             }
+           DispatchQueue.global().async {
+                                         DispatchQueue.main.async {
+                                                      self.tblView.reloadData() // refresh
+                                                    }
+                                     }
+               // let jsonData = JSON(responseData)
+            //self.arraySerach = (jsonData as? [[String:Any]])!
+            
+          
+           // self.arraySerach.append(responseData as! [String : Any])
+           // print(self.arraySerach)
+           // print(jsonData)
+            //print(self.arraySerach.count)
+           
            
                }
     }
@@ -43,7 +57,7 @@ class ViewController: UIViewController{
 }
 extension ViewController: UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return arraySerach.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tblView.dequeueReusableCell(withIdentifier: "DataCell", for: indexPath) as! DataCell
@@ -52,15 +66,15 @@ extension ViewController: UITableViewDelegate,UITableViewDataSource{
            {
              cell.idLbl.text = "Id : \(amount)"
            }
-        if let amount = dict["name"] as? Int
+        if let amount = dict["postId"] as? Int
            {
              cell.dataLbl.text = "User Id: \(amount)"
            }
-        cell.titleLbl.text = dict["email"] as? String
+        cell.titleLbl.text = dict["body"] as? String
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 124
-}
+          return 110
+  }
 }
